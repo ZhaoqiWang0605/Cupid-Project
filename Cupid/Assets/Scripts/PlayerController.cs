309f9e5adbb7dfc32927d0cff784c83c9a29ed76
Xueying Bai <xueying@Xueyings-MacBook-Pro.local>
2019-09-29 17:16:38 -0700

    Complete initial stage scene

---
+++ Cupid/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text scoreText;
+    public Text minuteText;
+    public Text secondText;
+
+    private double seconds = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        seconds -= Time.deltaTime;
+        SetTimeText();
+    }
+
+    void SetCountText()
+    {
+        scoreText.text = DataRecord.score.ToString();
+    }
+
+    void SetTimeText()
+    {
+        int minute = (int)seconds / 60;
+        int second = (int)seconds % 60;
+        minuteText.text = minute.ToString();
+        secondText.text = second.ToString();
+    }
+}
