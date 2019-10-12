e2e2e74afef0516111be0306121bfd77c777c4cd
Xueying Bai <xueying@Xueyings-MacBook-Pro.local>
2019-10-12 15:54:48 -0700

    Add background music for mainScene and StageMapScene

---
+++ Cupid/Assets/Scripts/ArrowShootingAudio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShootingAudio : MonoBehaviour
+{
+    public AudioClip musicClip;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = musicClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (UIForceArrowButtonController)
+    }
+}
