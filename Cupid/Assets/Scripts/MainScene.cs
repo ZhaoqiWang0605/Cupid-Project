de8d03fc65f865f188a9f3a58fe0040e929ce204
Zhaoqi Wang <zhaoqiwa@usc.edu>
2019-10-18 01:56:21 -0700

    beauty

--- Cupid/Assets/Scripts/MainScene.cs
+++ Cupid/Assets/Scripts/MainScene.cs
@@ -5,16 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MainScene : MonoBehaviour
 {
+    public GameObject bodyEyes_1;
+    public GameObject bodyEyes_2;
+    public bool isBodyEyes1;
+    public bool isBodyEyes2;
+    public float time_slot;
+    private float nextEffectTime;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        bodyEyes_1.SetActive(isBodyEyes1);
+        bodyEyes_2.SetActive(isBodyEyes2);
+        nextEffectTime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > nextEffectTime)
+        {
+            isBodyEyes1 = !isBodyEyes1;
+            isBodyEyes2 = !isBodyEyes2;
+            bodyEyes_1.SetActive(isBodyEyes1);
+            bodyEyes_2.SetActive(isBodyEyes2);
+            nextEffectTime += time_slot;
+        }
     }
 
     public void PlayButton()
--- Cupid/Assets/Scripts/SuccessDialog.cs
+++ Cupid/Assets/Scripts/SuccessDialog.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class SuccessDialog : MonoBehaviour
 {
 
-    public Text stars;
+    public GameObject star1;
+    public GameObject star2;
+    public GameObject star3;
 
     // Start is called before the first frame update
     void Start()
@@ -24,13 +26,21 @@ public class SuccessDialog : MonoBehaviour
     {
         if (starNumber == 1)
         {
-            stars.text = "✪";
-        } else if (starNumber == 2)
+            star1.SetActive(true);
+            star2.SetActive(false);
+            star3.SetActive(false);
+        } 
+        else if (starNumber == 2)
         {
-            stars.text = "✪ ✪";
-        } else if (starNumber == 3)
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(false);
+        }
+        else if (starNumber == 3)
         {
-            stars.text = "✪ ✪ ✪";
+            star1.SetActive(true);
+            star2.SetActive(true);
+            star3.SetActive(true);
         }
     }
 }
