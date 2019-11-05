9037435438ff352163315e937ab79eaf190ab35c
xxx663xxx <xxx663xxx@kimo.com>
2019-11-04 18:03:38 -0800

    Couple Refactor

--- Cupid/Assets/Scripts/CoupleController.cs
+++ Cupid/Assets/Scripts/CoupleController.cs
@@ -1,73 +1,72 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class CoupleController : MonoBehaviour
 {
-    public ParticleSystem heartEffect;
-    public ParticleSystem hitEffect;
-    public StageController stageController;
-    public SpriteRenderer sr;
-    public Sprite couple_inLove_image;
-    public float inLove_image_size;
-    public Audio audio;
-    public bool moveCupidAfterHit = true;
-
     private bool inLove = false;
+
+    public bool moveCupidAfterHit = true;
+    private Transform cupidPosAfterHit;
+
+    private StageController stageController;
     private UIForceArrowButtonController uIForceArrowButtonController;
+    private MoveableCameraController moveableCameraController;
+
+    private Animator animator;
+    private ParticleSystem heartEffect;
+    private ParticleSystem hitEffect;
+    private Audio arrowHitCoupleAudio;
 
     void Start()
     {
+        cupidPosAfterHit = GameObject.Find("CupidAfterHitPos").transform;
         stageController = GameObject.Find("StageController").GetComponent<StageController>();
         uIForceArrowButtonController = GameObject.Find("ArrowButton").GetComponent<UIForceArrowButtonController>();
-        sr = gameObject.GetComponent<SpriteRenderer>();
+        moveableCameraController = GameObject.Find("MoveableCamera").GetComponent<MoveableCameraController>();
+        animator = GetComponent<Animator>();
+        heartEffect = GameObject.Find("HeartEffect").GetComponent<ParticleSystem>();
+        hitEffect = GameObject.Find("HitEffect").GetComponent<ParticleSystem>();
+        arrowHitCoupleAudio = GameObject.Find("ArrowHitCoupleAudio").GetComponent<Audio>();
     }
-    
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         setInLove();
     }
 
-    public void setInLove() {
+    public void setInLove()
+    {
         if (!inLove)
         {
             inLove = true;
             stageController.UpdateScore(1000);
-            audio.PlayMusic();
+            GetComponent<BoxCollider2D>().enabled = false;
+
+            animator.SetBool("inLove", true);
+            arrowHitCoupleAudio.PlayMusic();
             hitEffect.Play();
+            heartEffect.Play();
+
             if (moveCupidAfterHit)
             {
-                uIForceArrowButtonController.moveCupidXto(transform.position.x);
+                uIForceArrowButtonController.moveCupidTo(cupidPosAfterHit);
             }
-            StartCoroutine(Sleep());
+            moveableCameraController.setFollow(transform);
         }
-        
+
     }
 
-    private void ChangeInLoveCoupleImage()
+    public bool isInLove()
     {
-        heartEffect.Play();
-        Debug.Log("CoupleController.inLove(): Set couple in love");
-        GetComponent<SpriteRenderer>().sprite = couple_inLove_image;
-        transform.localScale = new Vector3(inLove_image_size, inLove_image_size, 1.0f);
-    }
-
-    IEnumerator Sleep() {
-        yield return new WaitForSeconds(2);
-        ChangeInLoveCoupleImage();
-    }
-
-    public bool isInLove() {
         return inLove;
     }
 
     public void EnableFreeze()
     {
-
-        sr.color = new Color(0.3647f, 0.7843f, 1.0f, 1.0f);
+        animator.SetBool("freeze", true);
     }
 
     public void DisableFreeze()
     {
-        sr.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+        animator.SetBool("isFreezed", false);
     }
 }
