ba7dbc503bc130ded080746f92ba34ac45d60af0
Yifu Wang <w529399971@gmail.com>
2019-11-04 16:51:18 -0800

    fix freeze problem

--- Cupid/Assets/Scripts/LoadStage.cs
+++ Cupid/Assets/Scripts/LoadStage.cs
@@ -7,6 +7,12 @@ public class LoadStage : MonoBehaviour
     private void Awake()
     {
         //Debug.Log(PlayerPrefs.GetString("currentStage"));
+        Reset();
         Instantiate(Resources.Load(PlayerPrefs.GetString("currentStage")));
+    }
+
+    private void Reset()
+    {
+        Time.timeScale = 1;
     }
 }
