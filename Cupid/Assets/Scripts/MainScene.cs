b586fac9f43500c58d0bb8cda593dca21db28fea
kaixing <williams_wkx@hotmail.com>
2019-11-16 16:09:44 -0800

    change mainScene and stageMapScene UI

--- Cupid/Assets/Scripts/MainScene.cs
+++ Cupid/Assets/Scripts/MainScene.cs
@@ -5,18 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainScene : MonoBehaviour
 {
-    public GameObject bodyEyes_1;
-    public GameObject bodyEyes_2;
-    public bool isBodyEyes1;
-    public bool isBodyEyes2;
+    //public GameObject bodyEyes_1;
+    //public GameObject bodyEyes_2;
+    //public bool isBodyEyes1;
+    //public bool isBodyEyes2;
     public float time_slot;
     private float nextEffectTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        bodyEyes_1.SetActive(isBodyEyes1);
-        bodyEyes_2.SetActive(isBodyEyes2);
+        //bodyEyes_1.SetActive(isBodyEyes1);
+        //bodyEyes_2.SetActive(isBodyEyes2);
         nextEffectTime = 0.0f;
     }
 
@@ -25,10 +25,10 @@ public class MainScene : MonoBehaviour
     {
         if (Time.time > nextEffectTime)
         {
-            isBodyEyes1 = !isBodyEyes1;
-            isBodyEyes2 = !isBodyEyes2;
-            bodyEyes_1.SetActive(isBodyEyes1);
-            bodyEyes_2.SetActive(isBodyEyes2);
+            //isBodyEyes1 = !isBodyEyes1;
+            //isBodyEyes2 = !isBodyEyes2;
+            //bodyEyes_1.SetActive(isBodyEyes1);
+            //bodyEyes_2.SetActive(isBodyEyes2);
             nextEffectTime += time_slot;
         }
     }
